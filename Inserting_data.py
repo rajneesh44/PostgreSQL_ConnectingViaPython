0bbf609725d061c0fe68e065015db903cffe385a
rajneesh44 <rajneeshsharma338@gmail.com>
2020-01-28 20:46:12 +0530

    more changes in adding data

--- Inserting_data.py
+++ Inserting_data.py
@@ -7,7 +7,11 @@ print("Datbase connected/ opened successfully")
 cur = con.cursor()
 
 #execute will be doing the main work 
-cur.execute("INSERT INTO STUDENT (ADMISSION, NAME, AGE, COURSE, DEPARTMENT) VALUES (3420, 'JOHN', 21, 'COMPUTER SCIENCE', 'IT')")
+#cur.execute("INSERT INTO STUDENT (ADMISSION, NAME, AGE, COURSE, DEPARTMENT) VALUES (3420, 'JOHN', 21, 'COMPUTER SCIENCE', 'IT')")
+cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3419, 'Abel', 17, 'Computer Science', 'ICT')");
+cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3421, 'Joel', 17, 'Computer Science', 'ICT')");
+cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3422, 'Antony', 19, 'Electrical Engineering', 'Engineering')");
+cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3423, 'Alice', 18, 'Information Technology', 'ICT')");
 
 con.commit()
 print("Record inserted successfully")
---
+++ retrieving_data.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+con = psycopg2.connect(database="python_connect", user="postgres", password="rajneesh", host="127.0.0.1", port="5432")
+print("Database opened successfully")
+
+cur = con.cursor()
+cur.execute("SELECT admission, name, age, course, department from STUDENT")
+
+#fetching data
+rows = cur.fetchall()
+
+for row in rows:
+	print("ADMISSION =", row[0])
+	print("NAME=", row[1])
+	print("AGE =", row[2])
+	print("COURSE =", row[3])
+	print("DEPARTMENT =", row[4], "\n")
+
+print("Operation done successfully")
+con.close()
