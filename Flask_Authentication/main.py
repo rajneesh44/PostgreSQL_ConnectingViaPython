a59b3ee5f14432a4d2be3177020643d7368ccbec
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-10 23:55:37 +0530

    added main routing

---
+++ Flask_Authentication/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from . import db
+
+main = Blueprint('main',__name__)
+
+
+@main.route('/')
+def index():
+    return 'Index'
+
+
+@main.route('/profile')
+def profile():
+    return 'Profile'
+
