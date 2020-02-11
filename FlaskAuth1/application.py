3bf1b8d24a40703486f3ebc066bc4f61d52868ff
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-11 09:53:19 +0530

    main file

--- FlaskAuth1/application.py
+++ FlaskAuth1/application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from wtform import *
 
 app = Flask(__name__)
 
@@ -8,7 +8,12 @@ app.secret_key = 'replace later'
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template("index.html")
+
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Great Success"
+
+    return render_template("index.html",form = reg_form)
 
 
 if __name__=="__main__":
