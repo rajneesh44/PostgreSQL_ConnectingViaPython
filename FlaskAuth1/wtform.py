819ab7f51130c38b367db2c9b86b7cf26a77700f
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-12 22:46:19 +0530

    feat:Custom ValidationCreated

--- FlaskAuth1/wtform.py
+++ FlaskAuth1/wtform.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from models import *
 
 
 class RegistrationForm(FlaskForm):
@@ -10,7 +11,11 @@ class RegistrationForm(FlaskForm):
                                                          Length(min=4, max=25, message="Username must be b/w 4 and 25")])
     password = PasswordField('password_label', validators=[InputRequired(message="Password Required"),
                                                            Length(min=4, max=25, message="Password must be b/w 4 and 25")])
-
     confirm_pwd = PasswordField('confirm_label', validators=[InputRequired(message="Confirm password Required"),
                                                              EqualTo('password', message="passwords mus match")])
     submit_button = SubmitField('create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already Exists")
