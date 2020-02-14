19b62578cf1acc3dcecae555e3f719062ec4c0d8
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-15 02:41:50 +0530

    Hashed Password added

--- FlaskAuth1/application.py
+++ FlaskAuth1/application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from wtform import *
 from models import *
+from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 
@@ -14,7 +15,6 @@ db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
     reg_form = RegistrationForm()
     #Updates the database is validation successful
 
@@ -22,13 +22,16 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
+        hashed_pwd = pbkdf2_sha256.hash(password)
+
     #added the username validator in wtform.py file
         # #check username data
         # user_object = User.query.filter_by(username=username).first()
         # if user_object:
         #     return "Someone else has taken this username"
 
-        user = User(username=username, password=password)
+        # user = User(username=username, password=password)
+        user = User(username=username, password=hashed_pwd)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
