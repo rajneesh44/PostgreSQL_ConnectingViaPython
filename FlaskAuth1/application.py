63497180781bae652c4cfe31c77b6a6836123d08
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-14 22:51:19 +0530

    @LoginRoute Added

--- FlaskAuth1/application.py
+++ FlaskAuth1/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from wtform import *
 from models import *
 
@@ -16,6 +16,8 @@ db = SQLAlchemy(app)
 def index():
 
     reg_form = RegistrationForm()
+    #Updates the database is validation successful
+
     if reg_form.validate_on_submit():
         username = reg_form.username.data
         password = reg_form.password.data
@@ -29,8 +31,18 @@ def index():
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        return "Inserted into DB"
-    return render_template("index.html",form = reg_form)
+        return redirect(url_for('login'))
+
+    return render_template("index.html", form = reg_form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    #Allow login if validation success
+    if login_form.validate_on_submit():
+        return "Logged In, Finally!"
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == "__main__":
